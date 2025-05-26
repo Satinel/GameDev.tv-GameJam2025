3a930893b6ac3c6e568aa80c3593495b347a3413
Nimufu <angelonmyknees@gmail.com>
2025-05-26 01:06:43 -0300

    Hasty final victory screen which PROBABLY works

---
+++ Assets/Scripts/UI/VictorySplash.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class VictorySplash : MonoBehaviour
+{
+    [SerializeField] GameObject _window, _mainMenuButton;
+    bool _victory;
+    PlayerController _playerController;
+
+    void Start()
+    {
+        Enemy.OnEnemyKilled += Enemy_OnEnemyKilled;
+    }
+
+    void OnDestroy()
+    {
+        Enemy.OnEnemyKilled -= Enemy_OnEnemyKilled;
+    }
+
+    void Update()
+    {
+        if(!_victory) { return; }
+
+        if(EventSystem.current != _mainMenuButton)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(_mainMenuButton);
+        }
+    }
+
+    void Enemy_OnEnemyKilled(Enemy enemy)
+    {
+        if(!enemy.IsBoss) { return; }
+
+        _window.SetActive(true);
+        _victory = true;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_mainMenuButton);
+    }
+
+    public void LoadMainMenu()
+    {
+        _mainMenuButton.SetActive(false);
+        _playerController = FindFirstObjectByType<PlayerController>();
+        _playerController.transform.SetParent(transform); // This is a pretty clean way to remove DontDestroyOnLoad
+        SceneManager.LoadScene(0);
+    }
+}
